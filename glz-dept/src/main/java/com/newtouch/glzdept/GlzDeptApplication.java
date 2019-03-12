bcafc9edb3616bfb05de9acad07880cad6e238c8
Loren <990615749@qq.com>
2019-03-12 10:04:08 +0800

    修改数据库连接属性

--- glz-dept/src/main/java/com/newtouch/glzdept/GlzDeptApplication.java
+++ glz-dept/src/main/java/com/newtouch/glzdept/GlzDeptApplication.java
@@ -3,16 +3,23 @@ package com.newtouch.glzdept;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.newtouch")
 @MapperScan({"com.newtouch.*.dao"})
-public class GlzDeptApplication {
+public class GlzDeptApplication extends SpringBootServletInitializer {
 
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GlzDeptApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(GlzDeptApplication.class);
-        springApplication.run(args);
+        springApplication.run(GlzDeptApplication.class, args);
     }
 }
